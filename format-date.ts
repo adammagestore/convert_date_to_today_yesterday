06bd310d492a418d240a299c578470279a4d7358
Adam <adam@magestore.com>
2018-03-22 16:11:28 +0700

    fix issue of NaN date on ios device

--- format-date.ts
+++ format-date.ts
@@ -1,44 +1,53 @@
 /**
-     * 
-     * @param value 
+ * Fix the problem of display date time on Ios (ios is display as NaN)
+ */
+convertDateForIos(date) {
+    var arr = date.split(/[- :]/);
+    date = new Date(arr[0], arr[1]-1, arr[2], arr[3], arr[4], arr[5]);
+    return date;
+}
+
+/**
+     *
+     * @param value
      */
-    formatDate(value){
-        let order_date = new Date(value);
-        console.log(order_date);
+    formatDate(value) {
+        let order_date = this.convertDateForIos(value);
         let timeDiff = Date.now() - order_date.getTime();
-        let days = Math.ceil(timeDiff / (24*60*60*1000));
-        
+
+        let days = Math.ceil(timeDiff / (24 * 60 * 60 * 1000));
+
         let str: any;
-        switch(days){
-            case 1: 
+        switch (days) {
+            case 1:
                 str = "Today";
                 break;
-            case 2: 
+            case 2:
                 str = "Yesterday";
                 break;
-            case 3: 
+            case 3:
                 str = "2 days ago";
                 break;
-            case 4: 
+            case 4:
                 str = "3 days ago";
                 break;
-            case 5: 
+            case 5:
                 str = "4 days ago";
                 break;
-            case 6: 
+            case 6:
                 str = "5 days ago";
                 break;
-            case 7: 
+            case 7:
                 str = "6 days ago";
                 break;
-            case 8: 
+            case 8:
                 str = "7 days ago";
                 break;
             default:
                 //str = order_date.toLocaleString('en-US', { timeZone: 'UTC' });
                 str = this.formatDateToString(order_date);
                 break;
-        }   
+        }
         return str;
     }
 
